ad210affecb965ac878ee57d43a6d2e124ab378d
jordan houyon <116800565+Jordan-Houyon@users.noreply.github.com>
2022-11-20 20:05:49 +0100

    exo 56 ok

---
+++ main.js
@@ -0,0 +1,28 @@
+// --------- Ici le SWITCH ---------//
+
+let item=prompt("Quel accessoire utilisez-vous");
+
+switch(item){
+    case 'souris' :
+        alert("J'utilise un(e)" + ''+item)
+        break;
+    case "clavier":
+        alert("J'utilise un(e) " + item)
+        break;
+    case "ordinateur":
+        alert("J'utilise un(e) " + item)
+        break;
+    case "tapis":
+        alert("J'utilise un(e) " + item)
+        break;
+    case "imprimante":
+        alert("J'utilise un(e) " + item)
+        break;
+    case "ondulateur":
+        alert("J'utilise un(e) " + item)
+        break;
+    default:
+        alert("Accessoires inconnu");
+
+
+}
